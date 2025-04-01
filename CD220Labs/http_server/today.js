47a591c2803650ce562ee4c80ad7cc0a3389824d
Mefju Pe <johndoe@example.com>
2025-04-01 19:05:37 +0200

    tz update

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,6 +1,7 @@
  // Export a function named 'getDate' from the module
 module.exports.getDate = function getDate() {
     // Get the current date and time in the timezone "Australia/Brisbane"
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
+    let brisbaneTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
+    return brisbaneTime; // Return the formatted date and time
     return aestTime; // Return the formatted date and time
 };
